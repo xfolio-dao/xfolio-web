fc47fda06101ca514961c139fba61321ccdb6dea
exakoss <danielkossman@yahoo.com>
2021-09-30 21:10:34 -0400

    Rebuilt gas websocket with a hook

--- src/components/GasPrice.tsx
+++ src/components/GasPrice.tsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { commonStyles } from '../theme'
 import { toGwei } from '../utils/index'
+import useWebSocket from 'react-use-websocket'
 
 interface GasData {
     rapid: number,
@@ -11,39 +12,36 @@ interface GasData {
 
 const GasPrice:React.FC = () => {
 
-    const ws = new WebSocket('wss://www.gasnow.org/ws')
+    const socketUrl = 'wss://www.gasnow.org/ws'
+
+    const {
+        lastMessage,
+        lastJsonMessage,
+        readyState,
+        getWebSocket
+      } = useWebSocket(socketUrl, {
+        onOpen: () => console.log('opened'),
+        //Will attempt to reconnect on all close events, such as server shutting down
+        shouldReconnect: (closeEvent) => true,
+        retryOnError: true
+      });
+
+    // const ws = new WebSocket('wss://www.gasnow.org/ws')
 
     const [gasData,setGasData] = useState({rapid:0,fast:0,slow:0,standard:0})
 
-    const updateGasPriceData = (data:any) => {
-        console.log(data.gasPrices)
-        setGasData({...data.gasPrices})
-    }
-
-    ws.onopen = (evt) => {
-        // on connecting, do nothing but log it to the console
-        console.log('Connected')
-    }
-
-    ws.onmessage = evt => {
-        // listen to data sent from the websocket server
-        const data = JSON.parse(evt.data)
-        if (data.type) {
-            updateGasPriceData(data.data)
+    useEffect(() => {
+        console.log(lastJsonMessage)
+        if (lastJsonMessage) {
+            setGasData({...lastJsonMessage.data.gasPrices})
         }
-    }
-
-    ws.onclose = () => {
-        console.log('Disconnected')
-        // automatically try to reconnect on connection loss
-
-    }
+    },[lastJsonMessage])
 
     return(
         <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
-            <span>R: {toGwei(gasData.rapid)}</span>
-            <span>F: {toGwei(gasData.fast)}</span>
-            <span>St: {toGwei(gasData.standard)}</span>
+            <span style={commonStyles.tileText}>R: {toGwei(gasData.rapid)}</span>
+            <span style={commonStyles.tileText}>F: {toGwei(gasData.fast)}</span>
+            <span style={commonStyles.tileText}> St: {toGwei(gasData.standard)}</span>
         </div>
     )
 }
